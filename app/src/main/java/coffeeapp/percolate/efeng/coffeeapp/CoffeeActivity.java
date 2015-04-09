502cfe051a146ff4dbe1f5b52478a13557cb884e
Yijie Feng <yijiefeng@gmail.com>
2015-04-09 04:12:39 -0400

    refactor

--- app/src/main/java/coffeeapp/percolate/efeng/coffeeapp/CoffeeActivity.java
+++ app/src/main/java/coffeeapp/percolate/efeng/coffeeapp/CoffeeActivity.java
@@ -22,73 +22,44 @@ public class CoffeeActivity extends ActionBarActivity {
     private final static String apiEndpoint = "https://coffeeapi.percolate.com/api/coffee/?api_key=";
     private final static String apiKey = "WuVbkuUsCXHPx3hsQzus4SE";
 
-    TextView tv_results;
-    EditText etResponse;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.coffeeactivity_layout);
-
-        tv_results = (TextView) findViewById(R.id.tv_results);
-        etResponse = (EditText) findViewById(R.id.etResponse);
-
-        tv_results.setText("kjhkjhkjh");
+        setContentView(R.layout.coffeelist_layout);
 
         String url = apiEndpoint + apiKey;
-        Log.i("EEE", url);
+        Log.i("endpoint url", url);
 
         new apiCallTask().execute("https://coffeeapi.percolate.com/api/coffee/?api_key=WuVbkuUsCXHPx3hsQzus4SE");
-        //new apiCallTask().execute("http://hmkcode.appspot.com/rest/controller/get.json");
-
 
     }
 
-
     // AsyncTask for making a GET request
-    class apiCallTask extends AsyncTask<String, Void, JSONArray>{
-
-        private Exception exception;
+    protected class apiCallTask extends AsyncTask<String, Void, JSONArray> {
 
         protected JSONArray doInBackground(String... urls){
-
-            HttpURLConnection urlConnection = null;
             try{
                 // create connection
-                URL urlToRequest = new URL(urls[0]);
-
-                urlConnection = (HttpURLConnection)urlToRequest.openConnection();
-
-                urlConnection.setConnectTimeout(500);
-                urlConnection.setReadTimeout(500);
-
-                // handle issues
-                int statusCode = urlConnection.getResponseCode();
-                if (statusCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
-                    // handle unauthorized (if service requires user login)
-                } else if (statusCode != HttpURLConnection.HTTP_OK) {
-                    // handle any other errors, like 404, 500,..
-                }
+                URL request = new URL(urls[0]);
+                HttpURLConnection urlConnection = JsonUtil.createConnection(request);
 
                 // create JSON object from content
                 InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-
-                return new JSONArray(getResponseText(in));
+                return new JSONArray(JsonUtil.getResponseText(in));
 
             }catch(Exception e){
-                this.exception = e;
+                e.printStackTrace();
                 return null;
             }
-
         }
 
-        protected void onPostExecute(JSONArray result){
 
-            //ArrayList jsonlist = new ArrayList();
+        protected void onPostExecute(JSONArray result) {
+
             ArrayList<HashMap<String, String>> maplist = new ArrayList<HashMap<String, String>>();
 
             //store in a hashmap
-            for(int i=0;i<result.length();i++){
+            for (int i = 0; i < result.length(); i++) {
 
                 HashMap<String, String> map = new HashMap<String, String>();
                 JSONObject e = null;
@@ -108,28 +79,12 @@ public class CoffeeActivity extends ActionBarActivity {
                 }
             }
 
-//            Log.i("JSON RESPONSE", response);
-
-            for(int i=0; i<maplist.size(); i++){
+            for (int i = 0; i < maplist.size(); i++) {
                 HashMap<String, String> entry = maplist.get(i);
                 Log.i("JSON OUTPUT", entry.get("name"));
             }
 
-            //etResponse.setText(maplist.get(0));
         }
     }
 
-    private static String getResponseText(InputStream inStream) {
-        // very nice trick from
-        // http://weblogs.java.net/blog/pat/archive/2004/10/stupid_scanner_1.html
-        return new Scanner(inStream).useDelimiter("\\A").next();
-    }
-
-
-    //Method for parsing JSON response Object
-    public static void parseJson() {
-
-
-
-    }
 }
---
+++ app/src/main/java/coffeeapp/percolate/efeng/coffeeapp/JsonUtil.java
@@ -0,0 +1,40 @@
+package coffeeapp.percolate.efeng.coffeeapp;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class JsonUtil {
+
+    public static HttpURLConnection createConnection(URL urlToRequest){
+
+        HttpURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpURLConnection)urlToRequest.openConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        urlConnection.setConnectTimeout(500);
+        urlConnection.setReadTimeout(500);
+
+        return urlConnection;
+    }
+
+    public static String getResponseText(InputStream inStream) {
+        // very nice trick from
+        // http://weblogs.java.net/blog/pat/archive/2004/10/stupid_scanner_1.html
+        return new Scanner(inStream).useDelimiter("\\A").next();
+    }
+}
